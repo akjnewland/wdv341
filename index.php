03f977d08c65433dce2d4837d624f6001f98e343
Austin <newland.austink@gmail.com>
2026-09-08 14:08:18 -0500

    Adding an index page and introPHP Terms

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Homepage</title>
+</head>
+<body>
+  <?php
+  echo "Hello World!";
+  ?>
+
+  <hr>
+
+  <?php
+  $my_school = "DMACC"
+
+  echo $my_school;
+
+  echo "Data Type of $my_school is: " . gettype($my_school);
+  ?>
+</body>
+</html>
